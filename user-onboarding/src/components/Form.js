e37c62d6ce5dcea2526b2117d456b4dd7f15565d
Jose <Jrivera6869@yahoo.com>
2019-12-19 20:55:38 -0500

    working on displaying axios call

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,56 +1,103 @@
-import React from "react";
-import { withFormik, Form, Field, Formik } from "formik";
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field, Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const Forms = props => (
-  //   <div>
-  //     <h1>My Form</h1>
-  //     <Formik
-  //       initialValues={{ name: ``, email: ``, password: `` }}
-  //       onSubmit={(values, actions) => {}}>
-  //       {props => (
-  //         <form onSubmit={props.handleSubmit}>
-  //           <input
-  //             type='text'
-  //             onChange={props.handleChange}
-  //             onBlur={props.handleBlur}
-  //             value={props.values.name}
-  //             name='name'
-  //           />
-  //           {props.errors.name && <div id='feedback'>{props.errors.name}</div>}
-  //           <button type='submit'>Submit</button>
-  //         </form>
-  //       )}
-  //     </Formik>
-  //   </div>
-  <div>
-    <h1>My Form</h1>
-    <Formik
-      initialValues={{ name: ``, email: "", password: `` }}
-      onSubmit={(values, tools) => {
-        console.log(`things`, values, tools);
+const Forms = props => {
+  const [message, setMessage] = useState([]);
+  useEffect((tools) => {
+    console.log(`status has changed`, tools.status);
+    tools.status && setMessage(m => [...m, tools.status]);
+    {
+      console.log(tools.status, `hi`);
+    }
+  }, [tool.status]);
+
+  const SignupSchema = () =>
+    Yup.object().shape({
+      name: Yup.string().min(3, `Name Too Short!`),
+      email: Yup.string()
+        .email("Invalid email")
+        .required("Email Required"),
+      password: Yup.string().required(`Password required`),
+      terms: Yup.bool()
+        .test(
+          "consent",
+          "You have to agree with our Terms and Conditions!",
+          value => value === true
+        )
+        .required(`You have to agree with Terms of Service!`)
+    });
+
+  const handleSubmit = (values, tools) => {
+    
+    axios
+      .post(` https://reqres.in/api/users`, values)
+
+      .then(res => {
+        console.log(res, `success`);
+        console.log(values, `stuff`, tools, `yes`);
+        tools.setStatus(res.data);
         tools.resetForm();
-      }}>
-      {props => (
-        <Form>
-          <Field name='name' type='text' placeholder='name' />
+      })
+      .catch(err => console.log(err.response))
+      .finally();
+      
+  };
 
-          <Field name='email' type='text' placeholder='email' />
-
-          <Field name='password' type='password' placeholder='Password' />
-          <Field
-            id='Terms of Service'
-            type='checkbox'
-            name='Terms of Service'
-            checked={props.value}
-          />
-
-          <input type='submit' />
-        </Form>
-      )}
-    </Formik>
-  </div>
-);
+  return (
+    <div>
+      <h1>My Form</h1>
+      <Formik
+        initialValues={{ name: ``, email: "", password: `` }}
+        validationSchema={SignupSchema}
+        // validate={validate}
+        onSubmit={handleSubmit}>
+        {props => (
+          <Form className='formbody'>
+            <Field
+              className='formFields'
+              name='name'
+              type='text'
+              placeholder='name'
+            />
+            <ErrorMessage name='name' component='div' className='red' />
+            <Field
+              className='formFields'
+              name='email'
+              type='text'
+              placeholder='email'
+            />
+            <ErrorMessage name='email' component='div' className='red' />
+            <Field
+              className='formFields'
+              name='password'
+              type='password'
+              placeholder='Password'
+            />
+            <ErrorMessage name='password' component='div' className='red' />
+            <label htmlFor='terms'>
+              <Field
+                id='terms'
+                type='checkbox'
+                name='terms'
+                checked={props.value}></Field>
+              Agree with Terms of Service
+            </label>
+            &nbsp;
+            <input type='submit' />
+          </Form>
+        )}
+      </Formik>
+      <div>
+        {message.map(e => (
+          <div>
+            <p>{e.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Forms;
