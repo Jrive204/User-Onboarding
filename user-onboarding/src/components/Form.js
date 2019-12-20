1fba65141aa6b724d145215dd646361b11725ccb
Jose <Jrivera6869@yahoo.com>
2019-12-20 00:29:44 -0500

    fixed some stuff

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,18 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { withFormik, Form, Field, Formik, ErrorMessage } from "formik";
+import React, { useState } from "react";
+import { Form, Field, Formik, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-const Forms = props => {
+const Forms = ({ values, errors, touched, status }) => {
   const [message, setMessage] = useState([]);
-  useEffect((tools) => {
-    console.log(`status has changed`, tools.status);
-    tools.status && setMessage(m => [...m, tools.status]);
-    {
-      console.log(tools.status, `hi`);
-    }
-  }, [tool.status]);
+  console.log(status, `stat`);
 
+  //Submits ----
+
+  const handleSubmit = (values, { setStatus, resetForm }) => {
+    axios
+      .post(` https://reqres.in/api/users`, values)
+
+      .then(res => {
+        console.log(res.data, `success`);
+        setMessage([...message, values]);
+        setStatus(res.data);
+        console.log(values, `values`, status, `yes`);
+        resetForm();
+        console.log(status, `testing stuff please work!`);
+      })
+      .catch(err => console.log(err.response))
+      .finally();
+  };
+  // Checking Validations !! ----
   const SignupSchema = () =>
     Yup.object().shape({
       name: Yup.string().min(3, `Name Too Short!`),
@@ -29,22 +41,7 @@ const Forms = props => {
         .required(`You have to agree with Terms of Service!`)
     });
 
-  const handleSubmit = (values, tools) => {
-    
-    axios
-      .post(` https://reqres.in/api/users`, values)
-
-      .then(res => {
-        console.log(res, `success`);
-        console.log(values, `stuff`, tools, `yes`);
-        tools.setStatus(res.data);
-        tools.resetForm();
-      })
-      .catch(err => console.log(err.response))
-      .finally();
-      
-  };
-
+  // REturn STARTS HERE  - -------------
   return (
     <div>
       <h1>My Form</h1>
@@ -53,46 +50,52 @@ const Forms = props => {
         validationSchema={SignupSchema}
         // validate={validate}
         onSubmit={handleSubmit}>
-        {props => (
-          <Form className='formbody'>
-            <Field
-              className='formFields'
-              name='name'
-              type='text'
-              placeholder='name'
-            />
-            <ErrorMessage name='name' component='div' className='red' />
-            <Field
-              className='formFields'
-              name='email'
-              type='text'
-              placeholder='email'
-            />
-            <ErrorMessage name='email' component='div' className='red' />
-            <Field
-              className='formFields'
-              name='password'
-              type='password'
-              placeholder='Password'
-            />
-            <ErrorMessage name='password' component='div' className='red' />
-            <label htmlFor='terms'>
+        {({ values }) => {
+          return (
+            <Form className='formbody'>
               <Field
-                id='terms'
-                type='checkbox'
-                name='terms'
-                checked={props.value}></Field>
-              Agree with Terms of Service
-            </label>
-            &nbsp;
-            <input type='submit' />
-          </Form>
-        )}
+                className='formFields'
+                name='name'
+                type='text'
+                placeholder='name'
+              />
+              <ErrorMessage name='name' component='div' className='red' />
+              <Field
+                className='formFields'
+                name='email'
+                type='text'
+                placeholder='email'
+              />
+              <ErrorMessage name='email' component='div' className='red' />
+              <Field
+                className='formFields'
+                name='password'
+                type='password'
+                placeholder='Password'
+              />
+              <ErrorMessage name='password' component='div' className='red' />
+              <label htmlFor='terms'>
+                <Field
+                  id='terms'
+                  type='checkbox'
+                  name='terms'
+                  checked={values.terms}></Field>
+                Agree with Terms of Service
+              </label>
+              &nbsp;
+              <input type='submit' />
+            </Form>
+          );
+        }}
       </Formik>
+
+      {/* Map starts here !!!!! */}
       <div>
         {message.map(e => (
           <div>
             <p>{e.name}</p>
+            <p>{e.email}</p>
+            <p>{e.terms}</p>
           </div>
         ))}
       </div>
